51a559866bd722c713aa429359bec390f7467eca
EC2 Default User <ec2-user@ip-172-31-22-162.ec2.internal>
2023-03-01 18:21:40 +0000

    splitting into components

--- src/App.js
+++ src/App.js
@@ -1,236 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
 import {
-  Button,
   Flex,
   Heading,
-  Image,
-  Text,
-  TextField,
   View,
+  Grid,
+  Card,
+  Menu,
+  MenuItem,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
-import { listNotes, listPlayers } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-  createPlayer as createPlayerMutation,
-  deletePlayer as deletePlayerMutation,
-} from "./graphql/mutations";
+
+import AddListPlayers from './components/addlistplayers'
 
 const App = ({ signOut }) => {
-  const [notes, setNotes] = useState([]);
-  const [players, setPlayers] = useState([]);
-
-  useEffect(() => {
-    fetchNotes();
-    fetchPlayers();
-  }, []);
-
-  async function fetchNotes() {
-    const apiData = await API.graphql({ query: listNotes });
-    const notesFromAPI = apiData.data.listNotes.items;
-    await Promise.all(
-      notesFromAPI.map(async (note) => {
-        if (note.image) {
-          const url = await Storage.get(note.name);
-          note.image = url;
-        }
-        return note;
-      })
-    );
-    setNotes(notesFromAPI);
-  }
-  
-  async function fetchPlayers() {
-    const apiData = await API.graphql({ query: listPlayers });
-    const playersFromAPI = apiData.data.listPlayers.items;
-    await Promise.all(
-      playersFromAPI.map(async (player) => {
-        return player;
-      })
-    );
-    setPlayers(playersFromAPI);
-  }
-
-  async function createNote(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const image = form.get("image");
-    const data = {
-      name: form.get("name"),
-      description: form.get("description"),
-      image: image.name,
-    };
-    if (!!data.image) await Storage.put(data.name, image);
-    await API.graphql({
-      query: createNoteMutation,
-      variables: { input: data },
-    });
-    fetchNotes();
-    event.target.reset();
-  }
-  
-  async function createPlayer(event) {
-    event.preventDefault();
-    const form = new FormData(event.target);
-    const data = {
-      playername: form.get("playername"),
-      gamertag: form.get("gamertag"),
-      location: form.get("location"),
-      description: form.get("description"),
-    };
-    await API.graphql({
-      query: createPlayerMutation,
-      variables: { input: data },
-    });
-    fetchPlayers();
-    event.target.reset();
-  }
-
-  async function deleteNote({ id, name }) {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-    await Storage.remove(name);
-    await API.graphql({
-      query: deleteNoteMutation,
-      variables: { input: { id } },
-    });
-  }
-  
-  async function deletePlayer({ id, name }) {
-    const newPlayers = players.filter((player) => player.id !== id);
-    setPlayers(newPlayers);
-    await Storage.remove(name);
-    await API.graphql({
-      query: deletePlayerMutation,
-      variables: { input: { id } },
-    });
-  }
-
   return (
     <View className="App">
-      <Heading level={1}>My Notes App</Heading>
-      <View as="form" margin="3rem 0" onSubmit={createNote}>
-        <h3>Add Note</h3>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="name"
-            placeholder="Note Name"
-            label="Note Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Note Description"
-            label="Note Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <View
-            name="image"
-            as="input"
-            type="file"
-            style={{ alignSelf: "end" }}
-          />
-          <Button type="submit" variation="primary">
-            Create Note
-          </Button>
-        </Flex>
-      </View>
-      <br />
-      <View as="form" margin="3rem 0" onSubmit={createPlayer}>
-        <h3>Add Player</h3>
-        <Flex direction="row" justifyContent="center">
-          <TextField
-            name="playername"
-            placeholder="Player Name"
-            label="Player Name"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="gamertag"
-            placeholder="Gamertag"
-            label="Gamertag"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="location"
-            placeholder="Location"
-            label="Location"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <TextField
-            name="description"
-            placeholder="Player Description"
-            label="Player Description"
-            labelHidden
-            variation="quiet"
-            required
-          />
-          <Button type="submit" variation="primary">
-            Create Player
-          </Button>
-        </Flex>
-      </View>
-      <Heading level={2}>Current Notes</Heading>
-      <View margin="3rem 0">
-        {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            {note.image && (
-              <Image
-                src={note.image}
-                alt={`visual aid for ${notes.name}`}
-                style={{ width: 400 }}
-              />
-            )}
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
+      <Grid
+        columnGap="0.5rem"
+        rowGap="0.5rem"
+        templateRows="1fr"
+      >
+        <Card>
+          <Flex direction="row" justifyContent="space-between" alignItems="center">
+            <Heading level={1}>Three Kings</Heading>
+            <Menu menuAlign="end">
+              <MenuItem  onClick={signOut}>
+                Sign Out
+              </MenuItem>
+            </Menu>
           </Flex>
-        ))}
-      </View>
-      <br />
-      <Heading level={3}>Current Players</Heading>
-      <View margin="3rem 0">
-        {players.map((player) => (
-          <Flex
-            key={player.id || player.playername}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>{player.playername}</Text>
-            <Text as="span">{player.gamertag}</Text>
-            <Text as="span">{player.location}</Text>
-            <Text as="span">{player.description}</Text>
-            <Button variation="link" onClick={() => deletePlayer(player)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
-      </View>
-      <Button onClick={signOut}>Sign Out</Button>
+        </Card>
+
+        <AddListPlayers/>
+
+      </Grid>
+
     </View>
   );
 };
---
+++ src/components/addlistplayers.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react';
+import { API, Storage } from 'aws-amplify';
+import { Button, Flex, Heading, Text, TextField, View, Card } from '@aws-amplify/ui-react';
+import { listPlayers } from "../graphql/queries";
+import {
+  createPlayer as createPlayerMutation,
+  deletePlayer as deletePlayerMutation,
+} from "../graphql/mutations";
+
+const AddListPlayers = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
+  async function fetchPlayers() {
+    const apiData = await API.graphql({ query: listPlayers });
+    const playersFromAPI = apiData.data.listPlayers.items;
+    await Promise.all(
+      playersFromAPI.map(async (player) => {
+        return player;
+      })
+    );
+    setPlayers(playersFromAPI);
+  }
+
+  async function createPlayer(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const data = {
+      playername: form.get("playername"),
+      gamertag: form.get("gamertag"),
+      location: form.get("location"),
+      description: form.get("description"),
+    };
+    await API.graphql({
+      query: createPlayerMutation,
+      variables: { input: data },
+    });
+    fetchPlayers();
+    event.target.reset();
+  }
+
+  async function deletePlayer({ id, name }) {
+    const newPlayers = players.filter((player) => player.id !== id);
+    setPlayers(newPlayers);
+    await Storage.remove(name);
+    await API.graphql({
+      query: deletePlayerMutation,
+      variables: { input: { id } },
+    });
+  }
+
+  return (
+    <Card>
+      <View as="form" margin="3rem 0" onSubmit={createPlayer}>
+        <Heading level={3} margin="0 0 1rem">Add Player</Heading>
+        <Flex direction="row" justifyContent="center" alignItems="center">
+          <TextField
+            name="playername"
+            placeholder="Player Name"
+            label="Player Name"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="gamertag"
+            placeholder="Gamertag"
+            label="Gamertag"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="location"
+            placeholder="Location"
+            label="Location"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <TextField
+            name="description"
+            placeholder="Player Description"
+            label="Player Description"
+            labelHidden
+            variation="quiet"
+            required
+          />
+          <Button type="submit" variation="primary">
+            Create Player
+          </Button>
+        </Flex>
+      </View>
+        <Heading level={4}>Current Players</Heading>
+        <View margin="1rem 0">
+            {players.map((player) => (
+                <Flex key={player.id || player.playername} direction="row" justifyContent="center" alignItems="center">
+                    <Text as="strong" fontWeight={700}>
+                        {player.playername}
+                    </Text>
+                    <Text as="span">{player.gamertag}</Text>
+                    <Text as="span">{player.location}</Text>
+                    <Text as="span">{player.description}</Text>
+                    <Button variation="link" onClick={() => deletePlayer(player)}>
+                        Delete Player
+                    </Button>
+                </Flex>
+            ))}
+        </View>
+    </Card>
+    );
+}
+
+
+export default AddListPlayers;
