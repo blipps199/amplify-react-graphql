1364ff90e39772767f04e6b18ae4f7cf992820f8
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-03-11 16:09:39 +0000

    stuff

--- amplify/backend/function/deleteplayer/src/index.js
+++ amplify/backend/function/deleteplayer/src/index.js
@@ -9,15 +9,7 @@ const server = awsServerlessExpress.createServer(app);
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = (event) => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://www.example.com",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+exports.handler = (event, context) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
