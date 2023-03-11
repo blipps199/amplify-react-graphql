93f858404124e52a71e71b5469789b1c344ebe9d
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-03-11 21:20:02 +0000

    stuff

--- amplify/backend/function/deleteplayer/src/index.js
+++ amplify/backend/function/deleteplayer/src/index.js
@@ -5,12 +5,17 @@
  */
 exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
+    
+    const id = event.pathParameters.id;
+    
     return {
         statusCode: 200,
          headers: {
              "Access-Control-Allow-Origin": "*",
              "Access-Control-Allow-Headers": "*"
          }, 
-        body: JSON.stringify(JSON.stringify(event)),
+        body: JSON.stringify({
+          message: `Received ID: ${id}`
+        })
     };
 };
