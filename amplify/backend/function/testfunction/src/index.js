55a42c6c6d443840f141181959d94c6fa2414b6f
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-03-11 17:35:13 +0000

    stuff

--- amplify/backend/function/testfunction/src/index.js
+++ amplify/backend/function/testfunction/src/index.js
@@ -1,17 +1,14 @@
 
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
+export const handler = async (event) => {
+    const response = {
         statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.example.com",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify('Hello from Lambda!'),
     };
+    return response;
 };
