8cde44c71e16c8f3852d6dd633cd2b64d8b2b60a
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-03-11 18:46:51 +0000

    stuff

--- amplify/backend/function/testfunction/src/index.js
+++ amplify/backend/function/testfunction/src/index.js
@@ -5,7 +5,7 @@ export const handler = async (event) => {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://www.example.com",
+            "Access-Control-Allow-Origin": "https://master.d3jcbk48btvtpz.amplifyapp.com",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
         body: JSON.stringify('Hello from Lambda!'),
