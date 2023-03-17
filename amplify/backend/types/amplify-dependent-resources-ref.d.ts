3404e1b6124db0fb9e38e355a1401c9165492b3b
EC2 Default User <ec2-user@ip-172-31-19-139.ec2.internal>
2023-03-17 02:45:53 +0000

    auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,10 +10,8 @@ export type AmplifyDependentResourcesAttributes = {
     "amplifyreactgraphql803dc0ed": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
-      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
